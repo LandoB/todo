2f4085b3d5f348c54a897d3599eb7a3f7d0a47dc
Rolando Bernal <jrolandobernal@gmail.com>
2015-04-15 21:29:45 -0500

    Added Remove the DOM element as of Apr 15 2015 930 PM

--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -3,19 +3,22 @@ $(document).ready(function() { // Starts jQuery
  $(".deleteButton").click(function () {
    //console.log(this);
    var todoItemId = $(this)[0].id;           // Selector always going to return an index
-   console.log(todoItemId);
-  $.ajax({   // instead of GET
-       url: "/todo",
-       method: "DELETE",     // Then have to tell it what to delete
-       data: {
-           todo_id: todoItemId
-       }
-   },
-    function (response) {
-      console.log (response);
-    });
+      console.log(todoItemId);
+      $.ajax({   // instead of GET
+           url: "/todo",
+           method: "DELETE",     // Then have to tell it what to delete
+           data: {
+               todo_id: todoItemId
+           },
+           success : function(response) {
+
+                  //Remove the DOM element (you've done this before)
+                  $("#todo_"+ todoItemId).remove();
+                  alert("Item deleted succesfully.");
+
+                  } // end of success
+
+       });  // End of Ajax
   });
 
-
 }); // Ends jQuery
-
